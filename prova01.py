3b634165fc003eedfb1835378d9ec20f6014544b
Samir Xavier <105179524+ghostman000@users.noreply.github.com>
2024-08-20 23:14:57 -0300

    Add files via upload
    atividade 01 da infinity

---
+++ prova01.py
@@ -0,0 +1,8 @@
+##solicitando as medidas##
+base = int(input("informe a base do retângulo: "))
+altura = int(input("informe a altura do retângulo: "))
+
+##calculando a area##
+area = base*altura
+##informando a area para o usuario##
+print(f"A área do retangulo é: {area}")
