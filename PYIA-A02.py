c37fb99d8d1afc53154a8e38e732665b6b28e9ab
Samir Xavier <105179524+ghostman000@users.noreply.github.com>
2024-11-06 17:46:02 -0300

    Add files via upload

---
+++ PYIA-A02.py
@@ -0,0 +1,13 @@
+contato = {}
+
+
+nome = input("digite o nome do contato: ")
+tel = int(input("digite o numero de telefone fo contato: "))
+mail = input("digite o email do contato: ")
+
+contato['nome'] = nome
+contato['telefone'] = tel
+contato['email'] = mail
+
+print(contato)
+
