cd19bc368f43433043e16238699a6643c05dfaf7
Samir Xavier <105179524+ghostman000@users.noreply.github.com>
2025-02-13 17:58:48 -0300

    Add files via upload

--- [PYIA-A11].py
+++ [PYIA-A11].py
@@ -1,10 +1,3 @@
-#Crie uma classe base chamada Animal que tenha um método chamado falar, 
-# o qual imprime uma mensagem genérica, como "Este animal faz um som." 
-# Em seguida, crie duas subclasses chamadas Cachorro e Gato que herdem de Animal. 
-# Dentro de cada uma dessas subclasses, sobrescreva o método falar para que ele imprima uma mensagem específica para cada animal. 
-# Por exemplo, na classe Cachorro, o método falar pode imprimir "O cachorro late." e, na classe Gato, pode imprimir "O gato mia."
-
-
 class Animal:
     def __init__(self,nome):
         self.nome = nome
