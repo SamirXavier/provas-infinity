27d85ead99cc44dd05ee6759b958faf37099bfbc
Samir Xavier <105179524+SamirXavier@users.noreply.github.com>
2025-04-30 13:20:39 -0300

    Add files via upload

---
+++ loja.sql
@@ -0,0 +1,16 @@
+create database if not exists bdloja;
+use bdloja;
+
+create table if not exists Clientes (
+	ID int primary key auto_increment,
+	Nome varchar(50) not null,
+	Idade int not null,
+	Cidade varchar(25) not null
+);
+
+insert into Clientes (Nome,Idade,Cidade) values
+("Samir",17,"Salvador"),
+("Yan",18,"Salvador"),
+("Alisson",18,"Salvador"),
+("Antonio",18,"camaçari");
+SELECT * FROM bdloja.Clientes;
